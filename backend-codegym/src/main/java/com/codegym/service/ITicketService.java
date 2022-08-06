75eadedc61605d5840f3cbcfb7781ead7e9402a8
khanhtran10x <khanh150795@gmail.com>
2022-08-06 07:02:00 +0700

    first request

--- backend-codegym/src/main/java/com/codegym/service/ITicketService.java
+++ backend-codegym/src/main/java/com/codegym/service/ITicketService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ITicketService {
-    Page<Ticket> findAll(Pageable pageble);
+//    Page<Ticket> findAll(Pageable pageble);
 
     List<Ticket> findAll();
 
